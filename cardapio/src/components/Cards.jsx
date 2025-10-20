3835304402454ca0889593289d7288c01a529ce1
Italo-Emidio <italo.emidio7@gmail.com>
2025-10-19 21:49:03 -0300

    Adicionando cards através de um único componente

--- cardapio/src/components/Cards.jsx
+++ cardapio/src/components/Cards.jsx
@@ -1,26 +1,22 @@
 import Card from "./Card";
-import imgBurger from "../assets/pratos/burguer-picanha.jpeg";
-import imgFile from "../assets/pratos/enroladinho-mignon.jpeg";
+import { pratosPrincipais, sobremesas, bebidas } from "../assets/cardapio";
 
-function Cards() {
+const Cards = () => {
+  const itensCategoria = [pratosPrincipais, sobremesas, bebidas];
+  const pratos = itensCategoria[0];
+
   return (
     <div className="cards">
-      <Card
-        titulo="Burguer de Picanha"
-        descricao="Burguer de Picanha Angus, Queijo Cheddar, Crisp de Cebola e Geléia de
-        Bacon"
-        preco="R$ 44,90"
-        imagem={imgBurger}
-      />
-
-      <Card
-        titulo="Enroladinho de Mignon"
-        descricao="Finíssimas fatias de filé mignon enroladas no parmesão"
-        preco="R$ 64,90"
-        imagem={imgFile}
-      />
+      {pratos.map((prato) => (
+        <Card
+          titulo={prato.nome}
+          descricao={prato.descricao}
+          preco={prato.preco}
+          imagem={prato.imagem}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default Cards;
